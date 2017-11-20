a6af1752fa150ec5c02b2e8908f62ea22cfc0e8a
Jennifer Haddock <jentx123@hotmail.com>
2017-11-20 00:29:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+class Driver{
+  constructor(name, dateString){
+    this.name = name;
+    this.dateString = dateString;
+  }
 
+  startDate(){
+    
+  }
+
+  yearsExperienceFromBeginningOf(){
+    
+  }
+}
